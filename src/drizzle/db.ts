8cd95880d010d02439063c64133153c735ada7d8
Matin Sekhavat <113097369+matinsekhavat@users.noreply.github.com>
2025-05-01 20:00:58 +0330

    chore:Update db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,28 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { env } from "@/data/env";
-import * as schema from "./schema";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { env } from "@/data/env";
+// import * as schema from "./schema";
 
-export const db = drizzle({
-  schema,
-  connection: {
-    database: env.DB_NAME,
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-  },
+// export const db = drizzle({
+//   schema,
+//   connection: {
+//     database: env.DB_NAME,
+//     host: env.DB_HOST,
+//     user: env.DB_USER,
+//     password: env.DB_PASSWORD,
+//   },
+// });
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+import * as schema from "@/db/schema";
+import env from "@/lib/env";
+
+const pool = new Pool({
+	connectionString: env.DATABASE_URL,
 });
+
+// TODO: logger true
+export const db = drizzle(pool, { schema });
+
+export type DB = typeof db;
